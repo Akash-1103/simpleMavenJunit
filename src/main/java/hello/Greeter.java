0edfbeb459169d1eabd2976132d0cdd9a1c1159a
Akash-1103 <84493938+Akash-1103@users.noreply.github.com>
2022-12-24 09:58:57 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello CSMSS ...!! Hi , hello is the string needed here.Bye";
+    return "Hello ...!! Hi , hello is the string needed here.Bye";
   }
 }
